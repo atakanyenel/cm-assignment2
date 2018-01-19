662529ddb31c39c122e7c3e2e38a1b6ac042b555
Marius Momeu <momeu_marius@yahoo.com>
2018-01-19 23:47:37 +0100

    Uploaded client implementation with threads

--- code/client.py
+++ code/client.py
@@ -8,87 +8,96 @@ import commands
 import threading
 import time
 import subprocess
+import logging
 
 PORT=5555
 SLOW_IP="10.0.0.3"
 FAST_IP="10.1.0.3"
 
+def receiveFromServer(HOST_IP, e):
 
-def receiveFromServer(HOST_IP):
-    print "started new thread with %s" % HOST_IP
-    sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-   
-    server_address=(HOST_IP,PORT)
-    sock.connect(server_address)
-    file_received=open("received.txt","a")
-    file_at_hand=file_received.tell()
-    #print file_at_hand
-    amount_received=file_at_hand
-    sock.send(str(file_at_hand))
+	global done_receiving
+	done_receiving=False
+	
+	e.wait()
+	if done_receiving:
+		return
 
+	t_name=threading.current_thread().name	
+	
+	print t_name + ": starting"
+	
+	server_address=(HOST_IP,PORT)
 
-    try:
-        data=sock.recv(1024)
-        while data:
-            file_received.write(data)
-            amount_received+=len(data)
-            print amount_received
-            data=sock.recv(1024)
-        print file_received.tell()
-    finally:
-        "closing socket"
-        sock.close()
-        
-        file_received.close()
-    
-        sys.exit()
+	sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	
+	file_received=open("received.txt","a")
+		
+	try:
+		
+		sock.connect(server_address)
+		sock.send(str(file_received.tell()))
+		while True:
+			if not e.isSet():
+				
+				file_received.close()
+				sock.close()
+				
+				print t_name + ": waiting"
+				e.wait()
+	
+				file_received=open("received.txt","a")
+				
+				sock.connect(server_address)
+				sock.send(str(file_received.tell()))
+				
+			data=sock.recv(1024)
+			if data:
+				file_received.write(data)
+				print t_name + ": " + str(file_received.tell())
+			else:
+				print t_name + ": didn´t receive anything"
+				break
+	finally:
+	
+		done_receiving=True	
+		file_received.close()
+		sock.close()
+		print t_name + ": exiting"
+
 if __name__=="__main__":
-        if sys.argv[1]=="slow":
-            receiveFromServer(SLOW_IP)
-        elif sys.argv[1]=="fast":    
-            receiveFromServer(FAST_IP)
 
-        elif sys.argv[1]=="local":
-            print "local"
-            receiveFromServer("localhost")
-        elif sys.argv[1]=="main":
-            slow_process=subprocess.Popen(["echo","slow"])
-            fast_process=subprocess.Popen(["echo","fast"])
-            while True:
-                interface_num=commands.getstatusoutput("iwconfig |grep 'ssid' | wc -l")#[1][-1:]
-                #interface_num=commands.getstatusoutput("echo $[RANDOM%3+1]" )#[1][-1:]
-                print interface_num
-                interface_num=interface_num[1][-1:]
+	e_slow = threading.Event()
+	e_fast = threading.Event()
+	t_slow = threading.Thread(target=receiveFromServer, args=(SLOW_IP, e_slow))
+	t_fast = threading.Thread(target=receiveFromServer, args=(FAST_IP, e_fast))
 
-                if interface_num=="1": # only slow interface
-                    print "slow"
-                    if int(commands.getstatusoutput("ps aux | grep python | grep slow | wc -l")[1][-1:])>2:
-                        #slow process running
-                        print "slow process running"
-                        pass
-                    else:
-                        #slow process not running
-                        print "starting slow process"
-                        commands.getstatusoutput("kill -2 %s"% fast_process.pid)
-                        #kill fast process or do nothing
-                        if slow_process.poll()==False:
-                            slow_process=subprocess.Popen(["python","code/client.py","slow"]) #start slow process
-                            print "slow process started"
-                elif interface_num=="2":
-                    print "fast"
-                    if int(commands.getstatusoutput("ps aux | grep python | grep fast | wc -l")[1][-1:])>2:
-                        #fast process running
-                        pass
-                    else:
-                        
-                        commands.getstatusoutput("kill -2 %s" %slow_process.pid) 
-                        #kill slow process or do nothing
-                        if fast_process.poll()==False:
-                            fast_process=subprocess.Popen(["python","code/client.py","fast"])
-                            print "fast process started"
-                elif interface_num=="0":
-                    print "no_connection"
-                    commands.getstatusoutput("kill -2 %s" %fast_process.pid)
-                    commands.getstatusoutput("kill -2 %s" %slow_process.pid)
+	t_slow.start()
+	t_fast.start()
+	
+	while True:
+		interface_num=commands.getstatusoutput("iwconfig | grep 'ssid' | wc -l")
+		print interface_num
+		interface_num=interface_num[1][-1:]
 
-                time.sleep(3)
+		if interface_num=="1": # only slow interface
+			print "only the slow AP is in range"	
+			e_fast.clear()	
+			e_slow.set()
+
+		elif interface_num=="2":
+			print "detected a fast AP in range"
+			e_slow.clear()
+			e_fast.set()
+			
+		elif interface_num=="0":
+			print "no AP is in range"
+			e_slow.clear()
+			e_fast.clear()
+
+		if (not t_slow.isAlive()) or (not t_fast.isAlive()):
+			e_slow.set()
+			e_fast.set()
+			break;
+		
+		time.sleep(3)
